820fa4d9d0d7b3ad5ec383895fcb881edd7489df
othmane <gassabo22@gmail.com>
2023-03-16 03:34:51 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function is the entry point of a program. It is where the program starts executing when it is run. 
+ * 
+ * Return: is used to exit from a function and return a value to the caller of the function
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+                      
+}
+
+
